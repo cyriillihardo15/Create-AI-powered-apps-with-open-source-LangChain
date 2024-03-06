84af5877f3f03c09c4817a69e2e19d560c42e1d2
Lyhardo <71427434+cyriillihardo15@users.noreply.github.com>
2024-03-06 15:22:22 +0700

    Update hugface.py

--- hugface.py
+++ hugface.py
@@ -1,7 +1,7 @@
 # Menggunakan Hugging FaceHub
 import os
 from langchain_community.llms import HuggingFaceEndpoint
-os.environ["HUGGINGFACEHUB_API_TOKEN"] = "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"
+os.environ["HUGGINGFACEHUB_API_TOKEN"] = "PLACEKEYHERE"
 llm = HuggingFaceEndpoint(repo_id="google/flan-ul2")
 text = "Beritahu fakta menarik tentang kentang!"
 result = llm.invoke(text)
